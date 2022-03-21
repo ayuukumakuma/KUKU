64eee48b4cdf5a93d753af02db29fda9908580c3
Ayuu <ayuu.kumakuma@gmail.com>
2022-03-21 21:56:57 +0900

    ✨ add title&button&cross

--- front/pages/index.vue
+++ front/pages/index.vue
@@ -1,6 +1,22 @@
 <template>
 	<div>
-		<div id="title">KUKU</div>
+		<div
+			id="title"
+			class="sentence--text"
+		>KUKU</div>
+		<div id="cross">
+			<div id="cross-content" />
+		</div>
+		<div id="equal">
+			<div id="equal-content" />
+		</div>
+		<v-btn
+			id="start-btn"
+			class="rounded-xl sentence--text"
+			color="primary"
+		>
+			はじめる
+		</v-btn>
 	</div>
 </template>
 
@@ -13,14 +29,53 @@ export default {
 .container {
   padding: 0;
 }
+
 body {
 	position: relative;
 }
+
 #title {
 	font-size: 72px;
 	position: absolute;
 	top: 30%;
 	left: 50%;
+	z-index: 1;
 	transform: translate(-50%);
 }
+
+#start-btn {
+	font-size: 40px;
+	position: absolute;
+	left: 50%;
+	bottom: 20%;
+	transform: translate(-50%);
+	height: 100px;
+	width: 240px;
+}
+
+#cross {
+	position: absolute;
+	top: 55%;
+	left: 45%;
+	z-index: 0;
+	transform: rotate(-20deg);
+}
+
+#cross-content {
+	position: absolute;
+	background: var(--v-content-base);
+	height: 16px;
+	width: 170px;
+	transform: rotate(45deg);
+	&::after {
+		background: var(--v-content-base);
+		display: block;
+		content: "";
+		height: 16px;
+		width: 170px;
+		transform: rotate(-90deg);
+	}
+}
+
+
 </style>
