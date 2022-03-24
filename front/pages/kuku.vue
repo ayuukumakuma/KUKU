34e41ffaa12e35e65e18fc963b87bf3c0e0c5c1f
Ayuu <ayuu.kumakuma@gmail.com>
2022-03-24 18:27:13 +0900

    ✨ created UI

---
+++ front/pages/kuku.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <NavigationBar
+      page="KUKU"
+      back="./manu"
+    />
+    <v-container
+      id="tiles"
+    >
+      <v-row>
+        <v-col
+          class="pl-6 pr-3"
+        >
+          <OptionTiles />
+        </v-col>
+        <v-col
+          class="pl-3 pr-6"
+        >
+          <OptionTiles />
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col
+          class="pl-6 pr-3"
+        >
+          <OptionTiles />
+        </v-col>
+        <v-col
+          class="pl-3 pr-6"
+        >
+          <OptionTiles />
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+body {
+  position: relative;
+}
+
+#tiles {
+  position: absolute;
+  bottom: 24px;
+}
+</style>
