410299b6d4eadd7e18ef562d763990064c138326
Ayuu <ayuu.kumakuma@gmail.com>
2022-03-24 18:29:29 +0900

    🚧 creating component

---
+++ front/components/OptionTiles.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-card
+    height="152px"
+    width="152px"
+    color="#e0f7fa"
+    rounded="xl"
+  >
+    〇
+  </v-card>
+</template>
+
+<script>
+export default {}
+</script>
+<style lang="scss" scoped>
+
+</style>
