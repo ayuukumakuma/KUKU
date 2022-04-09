1aaf782854c4dd193d462e507539d4adda861d08
Ayuu <ayuu.kumakuma@gmail.com>
2022-04-09 23:14:47 +0900

    📝 delete test

--- front/pages/login.vue
+++ front/pages/login.vue
@@ -108,15 +108,6 @@ export default {
       },
     }
   },
-  mounted() {
-    console.log(process.env.apiKey)
-    console.log(process.env.authDomain)
-    console.log(process.env.projectId)
-    console.log(process.env.storageBucket)
-    console.log(process.env.messagingSenderId)
-    console.log(process.env.appId)
-    console.log(process.env.measurementId)
-  },
   methods: {
     loginGoogle() {
       const provider = new GoogleAuthProvider()
