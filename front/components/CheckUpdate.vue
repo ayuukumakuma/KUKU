6a505557c6acf330e51abb5d5c4901cf2d226423
Ayuu0721 <ayuu.kumakuma@gmail.com>
2022-05-23 09:46:29 +0900

    🚧 fixing sw

--- front/components/CheckUpdate.vue
+++ front/components/CheckUpdate.vue
@@ -1,21 +1,30 @@
 <template>
   <div>
-    <v-dialog v-model="isUpdate">
-      <v-card>
+    <v-dialog v-model="isUpdate" persistent>
+      <v-card v-if="!progress">
         <v-card-title>
           アップデートがあります<br />
           ボタンを押して更新してください
         </v-card-title>
-        <v-card-actions style="display: flex; justify-content: end;">
+        <v-card-actions style="display: flex; justify-content: end">
           <v-btn
-            style="font-size: 1.2rem;"
+            style="font-size: 1.2rem"
             class="rounded-lg"
             depressed
             color="content"
             large
+            @click="update()"
           >
             更新
           </v-btn>
+        </v-card-actions>
+      </v-card>
+      <v-card v-else>
+        <v-card-title>
+          更新中
+        </v-card-title>
+        <v-card-actions>
+          <v-progress-linear indeterminate color="primary" />
         </v-card-actions>
       </v-card>
     </v-dialog>
@@ -23,10 +32,28 @@
 </template>
 <script>
 export default {
+  data() {
+    return {
+      progress: false
+    }
+  },
   computed: {
     isUpdate() {
       return this.$store.getters['checkUpdate/isCheckUpdate']
-    }
-  }
+    },
+  },
+  methods: {
+    async update() {
+      this.progress = true
+      await window.navigator.serviceWorker
+        .getRegistrations()
+        .then((registrations) => {
+          for (const registration of registrations) {
+            registration.unregister()
+          }
+        })
+      window.location.reload(true)
+    },
+  },
 }
 </script>
--- front/layouts/default.vue
+++ front/layouts/default.vue
@@ -28,13 +28,7 @@ export default {
         workbox.addEventListener('installed', (event) => {
           if (event.isUpdate) {
             console.warn('最新版があります。')
-            window.navigator.serviceWorker
-              .getRegistrations()
-              .then((registrations) => {
-                for (const registration of registrations) {
-                  registration.unregister()
-                }
-              })
+            this.$store.dispatch('checkUpdate/change', true)
           }
         })
       }
